23ed2b94a4f883b1c67c6dd0cc785112dbe243b1
Gabriel Romero <garomerol.est.ucab.edu.ve>
2020-08-17 16:53:46 -0430

    act 4.3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-particlesJS('particles-js',
+$(document).ready(function(){
+
+  var windowWidth = $(window).width();
+
+
+  /*------Animacion inicial-----*/
+
+  if (windowWidth > 800) {
+    particlesJS('particles-js',
   
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 50,
         "density": {
           "enable": true,
           "value_area": 800
         }
       },
       "color": {
-        "value": "#000000"
+        "value": "#333"
       },
       "shape": {
         "type": "circle",
@@ -23,8 +31,8 @@ particlesJS('particles-js',
         },
         "image": {
           "src": "img/github.svg",
-          "width": 100,
-          "height": 100
+          "width": 50,
+          "height": 50
         }
       },
       "opacity": {
@@ -108,16 +116,127 @@ particlesJS('particles-js',
     },
     "retina_detect": true
   }
-
+  
 );
 
+  }
 
-$(document).ready(function(){
+  else{
+    particlesJS('particles-js',
+  
+    {
+      "particles": {
+        "number": {
+          "value": 40,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#333"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 2,
+            "height": 2
+          }
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.1,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 5,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 30,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": true,
+          "distance": 150,
+          "color": "#000000",
+          "opacity": 0.2,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 6,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "onclick": {
+            "enable": false,
+            "mode": "push"
+          },
+          "resize": false
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 40,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 200
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+      },
+      "retina_detect": true
+    }
+    
+  );
+  }
 
-  var windowWidth = $(window).width();
-
-  /*------Animacion inicial-----*/
-
+  
 
 
   $('.menu a').each(function(index){
